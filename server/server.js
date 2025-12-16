7177e7ef8cd58694572072319398998d7a3ce807
petros <petroshambirr@gmail.com>
2025-12-16 15:00:45 +0300

    server url fix

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ mongoose
 // CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+    'https://phone-call-frontend.onrender.com'],
     methods: "GET,POST,PUT,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
